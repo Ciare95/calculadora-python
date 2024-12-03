6157bee7850867342f134671271e8e0f391260f1
David <hcifuentes@gmail.com>
2024-12-02 21:50:37 -0500

    estructura básica de MVC

---
+++ main.py
@@ -0,0 +1,5 @@
+from controller.controlador import Controlador
+
+if __name__ == "__main__":
+    controlador = Controlador()
+    controlador.iniciar_sesion()
---
+++ model/database_connection.py
@@ -0,0 +1,28 @@
+import mysql.connector
+from mysql.connector import Error
+
+class DatabaseConnection:
+    def __init__(self):
+        try:
+            self.conn = mysql.connector.connect(
+                host = 'localhost',
+                user = 'root',
+                password = '',
+                database = 'calculadora'
+            )
+            if self.conn.is_connected():
+                print("Conexión exitosa a la base de datos")
+            
+        except Error as e:
+            print(f"Error al conectar la base de datos: {e}")
+            
+            
+                        
+    def Close_conection(self):
+        if self.conn.is_connected():
+            self.conn.close()
+            print("Conexión cerrada")
+            
+if __name__ == "__main__":
+    db = DatabaseConnection()
+    db.Close_conection()
